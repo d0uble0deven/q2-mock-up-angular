bdb962a409e720a4f2f28d78d88e69f60d457937
Dev Govindji <friendsofdev94@gmail.com>
2020-09-01 16:42:09 -0500

    test

---
+++ Desktop_Folder/angular_projects/grider/landing/src/app/weather/forecast.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { Observable, of } from 'rxjs'
+import { map, switchMap, pluck, filter, mergeMap } from 'rxjs/operators'
+
+interface OpenWeatherResponse {
+  list: {
+    dt_text: string
+    main: {
+      temp: number
+    }
+  }[]
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ForecastService {
+
+  private url = 'https://api.openweathermap.org/data/2.5/forecast'
+
+  constructor(private http: HttpClient) { }
+
+  getForecast() {
+    return this.getCurrentLocation().pipe(
+      map(coords => {
+        return new HttpParams()
+          .set('lat', String(coords.latitude))
+          .set('lon', String(coords.longitude))
+          .set('units', 'imperial')
+          .set('appid', 'b6ae99a1685d559d7f83f627b4a6490a')
+      }),
+      switchMap(params => this.http.get<OpenWeatherResponse>(this.url, { params })),
+      pluck('list'),
+      mergeMap(value => of(...value)),
+      filter((value, index) => index % 8 === 0)
+    )
+  }
+
+
+  getCurrentLocation() {
+    return new Observable<Coordinates>(observer => {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          observer.next(position.coords)
+          observer.complete()
+        },
+        (err) => observer.error(err)
+      )
+
+    })
+
+  }
+
+}
