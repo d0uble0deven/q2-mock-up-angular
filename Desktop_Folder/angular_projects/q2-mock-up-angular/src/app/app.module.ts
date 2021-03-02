afc7e1b6897cddd6e571a4730c9806527ced1ed6
Dev Govindji <friendsofdev94@gmail.com>
2021-03-02 15:19:33 -0600

    implements react form

--- Desktop_Folder/angular_projects/q2-mock-up-angular/src/app/app.module.ts
+++ Desktop_Folder/angular_projects/q2-mock-up-angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +20,7 @@ import { ContactFormComponent } from './contact-form/contact-form.component'
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
