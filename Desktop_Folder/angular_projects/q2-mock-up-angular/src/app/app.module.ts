b57b3535ebcad177acb27d8655a7ae45a6b01611
Dev Govindji <friendsofdev94@gmail.com>
2021-03-02 13:58:17 -0600

    implemented a template-driven form

--- Desktop_Folder/angular_projects/q2-mock-up-angular/src/app/app.module.ts
+++ Desktop_Folder/angular_projects/q2-mock-up-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { ContactFormComponent } from './contact-form/contact-form.component'
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- Desktop_Folder/angular_projects/q2-mock-up-angular/src/app/contact-form/contact-form.component.ts
+++ Desktop_Folder/angular_projects/q2-mock-up-angular/src/app/contact-form/contact-form.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
+  favoriteColor = ''
+
   constructor() { }
 
   ngOnInit(): void {
